010de0e307e997bd14259d5b3be6f0f8f5823d9c
Jean Minet <jean.minet@gmail.com>
2021-06-20 20:58:39 +0200

    Compute probabilities of each country being qualified

---
+++ README.md
@@ -0,0 +1,26 @@
+> python .\euro2021.py
+Probability of team being qualified (2021-06-20)
+Pays-Bas : 100.0 %
+Belgique : 100.0 %
+Pays de Galles : 100.0 %
+Italie : 100.0 %
+République tchèque : 98.7 %
+Angleterre : 98.4 %
+Suisse : 98.4 %
+France : 97.7 %
+Suède : 97.1 %
+Portugal : 70.2 %
+Allemagne : 66.4 %
+Finlande : 63.0 %
+Slovaquie : 62.3 %
+Ukraine : 62.0 %
+Autriche : 60.8 %
+Russie : 56.5 %
+Espagne : 49.0 %
+Croatie : 45.2 %
+Hongrie : 44.0 %
+Pologne : 42.8 %
+Ecosse : 42.7 %
+Danemark : 28.4 %
+Macédoine du Nord : 16.7 %
+Turquie : 0.0 %
--- euro2021.py
+++ euro2021.py
@@ -1,4 +1,5 @@
 import random
+import datetime
 
 Z = 0
 n = 10000
@@ -24,45 +25,54 @@ def flatten(X):
             F[4*i+j] = X[i][j]
     return F
 
+# 6 groups of 4 teams
+G = [[]*4 for i in range(6)]
+G[0] = ["Turquie", "Italie", "Pays de Galles", "Suisse"]
+G[1] = ["Danemark", "Finlande", "Belgique", "Russie"]
+G[2] = ["Pays-Bas", "Ukraine", "Autriche", "Macédoine du Nord"]
+G[3] = ["Angleterre", "Croatie", "Ecosse", "République tchèque"]
+G[4] = ["Espagne", "Suède", "Pologne", "Slovaquie"]
+G[5] = ["Hongrie", "Portugal", "France", "Allemagne"]
+Gf = flatten(G)
+
+qualified = [0]*24
 
 for t in range(n):
-
-    G = [[]*4 for i in range(6)]
-    G[0] = ["Turquie", "Italie", "Pays de Galles", "Suisse"]
-    G[1] = ["Danemark", "Finlande", "Belgique", "Russie"]
-    G[2] = ["Pays-Bas", "Ukraine", "Autriche", "Macédoine du Nord"]
-    G[3] = ["Angleterre", "Croatie", "Ecosse", "République tchèque"]
-    G[4] = ["Espagne", "Suède", "Pologne", "Slovaquie"]
-    G[5] = ["Hongrie", "Portugal", "France", "Allemagne"]
 
     P = [[0]*4 for i in range(6)]
     S = [[[0]*3 for i in range(4)] for j in range(6)]
 
-    S[0][0] = [0,0,-1]
-    S[0][1] = [3,3,-1]
-    S[0][2] = [1,2,-1]
-    S[0][3] = [1,0,-1]
+    # Group 1
+    S[0][0] = [0,0,1]
+    S[0][1] = [3,3,1]
+    S[0][2] = [1,2,0]
+    S[0][3] = [1,0,3]
 
+    # Group 2
     S[1][0] = [0,1,-1]
     S[1][1] = [1,0,-1]
     S[1][2] = [3,2,-1]
     S[1][3] = [0,1,-1]
 
+    # Group 3
     S[2][0] = [3,2,-1]
     S[2][1] = [2,2,-1]
     S[2][2] = [3,0,-1]
     S[2][3] = [1,1,-1]
 
+    # Group 4 
     S[3][0] = [1,0,-1]
     S[3][1] = [0,1,-1]
     S[3][2] = [0,0,-1]
     S[3][3] = [2,1,-1]
 
+    # Group 5
     S[4][0] = [0,1,-1]
     S[4][1] = [0,1,-1]
     S[4][2] = [1,1,-1]
     S[4][3] = [2,0,-1]
 
+    # Group 6
     S[5][0] = [0,1,-1]
     S[5][1] = [3,2,-1]
     S[5][2] = [1,1,-1]
@@ -76,28 +86,27 @@ for t in range(n):
         P = match(P,S,p,1,0,2)
         P = match(P,S,p,1,1,3)
 
-    for i in range(1):
-        # Random draw of still unplayed matches
-        for p in range(6):
-            for e in range(4):
-                for j in range(3):
-                    if S[p][e][j] == -1:
-                        S[p][e][j] = random.randint(0,7)
-        
-        #S[5][2][2] = 0
-        #S[5][1][2] = 7
+    # Random draw of still unplayed matches
+    for p in range(6):
+        for e in range(4):
+            for j in range(3):
+                if S[p][e][j] == -1:
+                    S[p][e][j] = random.randint(0,7)
 
-        for p in range(6):
-            # journée 3
-            P = match(P,S,p,2,0,3)
-            P = match(P,S,p,2,1,2)
+    for p in range(6):
+        # journée 3
+        P = match(P,S,p,2,0,3)
+        P = match(P,S,p,2,1,2)
 
-        Gf = flatten(G)
-        P = flatten(P)
 
-        rank = sorted(range(24), key=lambda k: P[k])
-        A = [Gf[i] for i in rank].index("France")
-        if A <= 7:
-            Z += 1
+    P = flatten(P)
 
-print(Z/n*100)
+    rank = sorted(range(24), key=lambda k: P[k])
+    for r in rank[8:24]:
+        qualified[r] += 1
+    
+rank = sorted(range(24), key=lambda k: qualified[k])
+
+print("Probability of team being qualified ({})".format(datetime.date.today()))
+for i in rank[::-1]:
+    print("{} : {:.1f} %".format(Gf[i], qualified[i] / n * 100))
